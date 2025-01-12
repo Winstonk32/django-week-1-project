d08af15723caa7ab80e0f064d0a5ae72898dcc93
Winstonk32 <winstonkigonya4@gmail.com>
2025-01-12 05:14:52 +0300

    changes

--- CalorieClaculator/CalorieClaculator/settings.py
+++ CalorieClaculator/CalorieClaculator/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'CalorieClaculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR/'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,19 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
-]
+STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER='***'
-EMAIL_HOST_PASSWORD='***'
--- CalorieClaculator/count/urls.py
+++ CalorieClaculator/count/urls.py
@@ -1,29 +1,9 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path('user/',views.userPage,name='userPage'), #userPage
-    path('product/',views.fooditem,name='fooditem'),
-    path('createfooditem/',views.createfooditem,name='createfooditem'),
-    path('register/',views.registerPage,name='register'),
-    path('login/',views.loginPage,name='login'),
-    path('logout/',views.logoutUser,name='logout'),
-    path('addFooditem/',views.addFooditem,name='addFooditem'),
-     path('reset_password/',
-         auth_views.PasswordResetView.as_view(),
-         name="reset_password"),
-
-    path('reset_password_sent/',
-         auth_views.PasswordResetDoneView.as_view(),
-         name="password_reset_done"),
-
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
-         name="password_reset_confirm"),
-
-    path('reset_password_complete/',
-         auth_views.PasswordResetCompleteView.as_view(),
-         name="password_reset_complete"),
+    path ('', views.add_food, name= 'add_food'),
+    path('view-food/', views.view_food, name='view_food'),
+    path('remove/<int:food_id>/', views.remove_food, name='remove_food'),
+    path('reset/', views.reset_calories, name='reset_calories'),
 ]
